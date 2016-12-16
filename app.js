725e3eae153bc9a0d8ec3e0232727a5bcd6978eb
Tinjo <Tinjo@Martins-MacBook-Pro.local>
2016-12-16 13:06:33 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var path = require('path');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Server is running on port  " + port);
+});
